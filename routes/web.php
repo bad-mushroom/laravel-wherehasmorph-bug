f2eaa1c30ddc5edc74b88ea3dd888d47f23723e0
Chris Sprague <chris@chaoscontrol.org>
2020-04-23 13:36:19 -0400

    Add comment and context for error

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    /**
+     * App\Car exists in DB_02
+     * App\Vehicle exists in DB_01
+     *
+     * Vehicle is polymorphic and can store Car or Truck models from DB_02 via their UUID.
+     *
+     * Calling `whereHasMorph` assumes App\Car exists in DB_01 despite the connection
+     * that is specified on the model.
+     *
+     * SQLSTATE[42S02]: Base table or view not found: 1146 Table 'DB_01.cars' doesn't exist
+     * (SQL: select * from `vehicles` where ((`vehicles`.`drivable_type` = App\Car and exists
+     * (select * from `cars` where `vehicles`.`drivable_id` = `cars`.`id` and `color` = Blue))))
+     */
     $results = \App\Vehicle::whereHasMorph('drivable', [\App\Car::class], function ($query) {
         $query->where('color', 'Blue');
     });
-
-    // $results = \App\Vehicle::whereHasMorph('drivable', ['*'], function ($query) {
-    //     $query->where('color', 'Blue');
-    // });
 
     // Exception (as expected): `Please use whereHasMorph() for MorphTo relationships.`
     // $results = \App\Vehicle::whereHas('drivable', function ($query) {
